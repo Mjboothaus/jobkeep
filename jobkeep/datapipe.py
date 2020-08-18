2449a9d138d16ff9900496860d11771c2dab05bb
Michael Booth <mjboothaus@michaels-air.home>
2020-08-18 18:01:11 +1000

    Initial setup of datapipe

--- jobkeep/datapipe.py
+++ jobkeep/datapipe.py
@@ -3,15 +3,24 @@
 __all__ = ['load_and_cache_raw_data']
 
 # Internal Cell
-import os
+import os.path
 import pandas as pd
-import activityio as aio
-from dateutil.parser import parse
-import datetime as dt
+#from dateutil.parser import parse
+#import datetime as dt
 
 # Cell
 
 # TODO: use st.cache() and also look to pre-load and cache/feather data (or similar) - NB: use of @st.cache() below didn't work
 def load_and_cache_raw_data():
-    DATA_PATH = '/Users/mjboothaus/iCloud/Code/Github/jobkeep/data'
-    return None
+    converters = {'Postcode': str}
+    DATA_PATH = '/Users/mjboothaus/iCloud/Code/Github/jobkeep/data/'
+    DATA_FILE = 'JobKeeper-data-20200731.xlsx'
+    CACHE_FILE = DATA_PATH + 'cache/' + DATA_FILE.replace('.xlsx', '.feather')
+
+    if os.path.isfile(CACHE_FILE):
+        df = pd.read_feather(CACHE_FILE)
+        print('Using cached data file...')
+    else:
+        df = pd.read_excel(DATA_PATH + DATA_FILE, converters=converters, sheet_name='Data', skiprows=1, skipfooter=2)
+        df.to_feather(CACHE_FILE)
+    return df
